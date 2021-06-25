5937bcfe8e58ca793b850bd39a42ddf5a08824fa
AhnHyobeom <dksgyqja14@gmail.com>
2021-06-25 17:15:47 +0900

    init commit

---
+++ MultiChat/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+using System.Net.Sockets;
+
+namespace MultiChat
+{
+
+    public partial class Form1 : Form
+    {
+        delegate void AppendTextDelegate(Control ctrl, string s);
+        AppendTextDelegate _textAppender;
+        Socket mainSock;
+        IPAddress thisAddress;
+
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                mainSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+                _textAppender = new AppendTextDelegate(AppendText);
+            } catch(Exception e)
+            {
+
+            }
+        }
+
+        void AppendText(Control ctrl, string s)
+        {
+            if (ctrl.InvokeRequired) ctrl.Invoke(_textAppender, ctrl, s);
+            else
+            {
+                string source = ctrl.Text;
+                ctrl.Text = source + Environment.NewLine + s;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IPHostEntry he = Dns.GetHostEntry(Dns.GetHostName());
+
+            // 처음으로 발견되는 ipv4 주소를 사용한다.
+            foreach (IPAddress addr in he.AddressList)
+            {
+                if (addr.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    thisAddress = addr;
+                    break;
+                }
+            }
+
+            // 주소가 없다면..
+            if (thisAddress == null)
+                // 로컬호스트 주소를 사용한다.
+                thisAddress = IPAddress.Loopback;
+
+            txtAddress.Text = thisAddress.ToString();
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            int port;
+            if (!int.TryParse(txtPort.Text, out port))
+            {
+                MsgBoxHelper.Error("포트 번호가 잘못 입력되었거나 입력되지 않았습니다.");
+                txtPort.Focus();
+                txtPort.SelectAll();
+                return;
+            }
+
+            // 서버에서 클라이언트의 연결 요청을 대기하기 위해
+            // 소켓을 열어둔다.
+            IPEndPoint serverEP = new IPEndPoint(thisAddress, port);
+            mainSock.Bind(serverEP);
+            mainSock.Listen(10);
+
+            // 비동기적으로 클라이언트의 연결 요청을 받는다.
+            mainSock.BeginAccept(AcceptCallback, null);
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
