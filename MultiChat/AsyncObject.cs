4213289774c3d80ba06229b151f39d0d351a4123
AhnHyobeom <dksgyqja14@gmail.com>
2021-06-28 15:31:40 +0900

    Server 거의 완성

---
+++ MultiChat/AsyncObject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace MultiChat
+{
+    public class AsyncObject
+    {
+        private byte[] buffer;
+        private Socket workingSocket;
+        private readonly int bufferSize;
+
+        public byte[] Buffer { get => buffer; set => buffer = value; }
+        public Socket WorkingSocket { get => workingSocket; set => workingSocket = value; }
+
+        public int BufferSize => bufferSize;
+
+        public AsyncObject(int bufferSize)
+        {
+            this.bufferSize = bufferSize;
+            this.Buffer = new byte[this.bufferSize];
+        }
+
+        public void ClearBuffer()
+        {
+            Array.Clear(Buffer, 0, BufferSize);
+        }
+    }
+}
---
+++ MultiChatClient/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace MultiChatClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtHistory = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtTTS = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.lblTTS = new System.Windows.Forms.Label();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtHistory
+            // 
+            this.txtHistory.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtHistory.Location = new System.Drawing.Point(23, 70);
+            this.txtHistory.Multiline = true;
+            this.txtHistory.Name = "txtHistory";
+            this.txtHistory.Size = new System.Drawing.Size(739, 367);
+            this.txtHistory.TabIndex = 17;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnSend.Location = new System.Drawing.Point(636, 451);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(124, 41);
+            this.btnSend.TabIndex = 16;
+            this.btnSend.Text = "보내기";
+            this.btnSend.UseVisualStyleBackColor = true;
+            // 
+            // txtTTS
+            // 
+            this.txtTTS.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtTTS.Location = new System.Drawing.Point(173, 453);
+            this.txtTTS.Name = "txtTTS";
+            this.txtTTS.Size = new System.Drawing.Size(444, 35);
+            this.txtTTS.TabIndex = 15;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnStart.Location = new System.Drawing.Point(638, 17);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(124, 41);
+            this.btnStart.TabIndex = 14;
+            this.btnStart.Text = "시작";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtPort.Location = new System.Drawing.Point(464, 19);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(153, 35);
+            this.txtPort.TabIndex = 13;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtAddress.Location = new System.Drawing.Point(148, 19);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(170, 35);
+            this.txtAddress.TabIndex = 12;
+            // 
+            // lblTTS
+            // 
+            this.lblTTS.AutoSize = true;
+            this.lblTTS.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblTTS.Location = new System.Drawing.Point(23, 456);
+            this.lblTTS.Name = "lblTTS";
+            this.lblTTS.Size = new System.Drawing.Size(144, 24);
+            this.lblTTS.TabIndex = 11;
+            this.lblTTS.Text = "보낼 텍스트";
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblPort.Location = new System.Drawing.Point(334, 22);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(119, 24);
+            this.lblPort.TabIndex = 10;
+            this.lblPort.Text = "포트 번호";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblAddress.Location = new System.Drawing.Point(23, 22);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(119, 24);
+            this.lblAddress.TabIndex = 9;
+            this.lblAddress.Text = "서버 주소";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 504);
+            this.Controls.Add(this.txtHistory);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtTTS);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.lblTTS);
+            this.Controls.Add(this.lblPort);
+            this.Controls.Add(this.lblAddress);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtHistory;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtTTS;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label lblTTS;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.Label lblAddress;
+    }
+}
+
